a8cd26bda2344db86d4c63377cf575eb6089d04f
ljs-1989 <725459481@qq.com>
2019-03-10 22:54:58 +0800

    Initial commit

---
+++ src/main/java/com/example/mylibrary/TestLib.java
@@ -0,0 +1,17 @@
+package com.example.mylibrary;
+
+/**
+ * Created by liujs on 2019-03-10.
+ */
+
+public class TestLib {
+    private String mTest = "sfasfasfa";
+
+    public String getmTest() {
+        return mTest;
+    }
+
+    public void setmTest(String mTest) {
+        this.mTest = mTest;
+    }
+}
